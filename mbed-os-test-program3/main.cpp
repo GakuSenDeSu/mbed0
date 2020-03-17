f48769cedb2ef047dc8b0d8c8dd42e3cd5726298
greenballow@gmail.com <greenballow@gmail.com>
2020-03-17 22:58:47 +0800

    M

--- mbed-os-test-program3/main.cpp
+++ mbed-os-test-program3/main.cpp
@@ -8,9 +8,19 @@ int Led(DigitalOut& ledpin);
 int main()
 {
    redLED = 1;
+
    greenLED = 1;
+   
    while (true) {
-       Led(redLED);
-       Led(greenLED);
+       //red LED blink for 3 times
+       for(int i=0; i<6; ++i) {
+       redLED=!redLED;
+       wait(0.2f);
+       }
+       //green LED blink for 2 times
+       for (int i=0; i<2; ++i) {
+       greenLED=!greenLED;
+       wait(0.2f);
+       }
    }
 }
