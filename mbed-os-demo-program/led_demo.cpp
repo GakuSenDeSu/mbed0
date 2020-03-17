c4d5de152707ffe496d6f76e63a12e50f2347826
greenballow@gmail.com <greenballow@gmail.com>
2020-03-17 19:36:44 +0800

    Demo

--- mbed-os-demo-program/led_demo.cpp
+++ mbed-os-demo-program/led_demo.cpp
@@ -5,12 +5,18 @@ void Led(DigitalOut& ledpin)
 
 {
 
-   for(int i=0; i<2; ++i) { //blink for 2 times
+   for(int i=0; i<6; ++i) { //blink for 6 times
 
        ledpin = !ledpin; // toggle led
 
        wait(0.2f);
 
+   }
+   for(int j=0; i<8; ++i) { //blink for 4 times
+   
+       ledpin = !ledpin;
+
+       wait(0.2f);
    }
 
 }
